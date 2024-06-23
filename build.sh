9ca2bc7f4998da268811316e579c6d034608fca0
ashwin31081999 <163541950+ashwin31081999@users.noreply.github.com>
2024-06-23 21:47:35 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,28 +1,9 @@
 #!/bin/bash
 
-# Get the current branch name
-CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-
-# Define the image name
-IMAGE_NAME="my-nginx:latest"
-
-# Delete existing Docker image if it exists
-if [[ $(docker images -q $IMAGE_NAME 2> /dev/null) != "" ]]; then
-  docker rmi -f $IMAGE_NAME
-fi
+# Remove any existing image with the same name
+docker rmi -f my-nginx:latest
 
 # Build the Docker image
-docker build -t $IMAGE_NAME .
+docker build -t my-nginx:latest .
 
-# Tag the image for the appropriate repository
-if [[ $CURRENT_BRANCH == "master" ]]; then
-  docker tag $IMAGE_NAME ashwin31081999/prod:latest
-  echo "Tagged image as ashwin31081999/prod:latest"
-elif [[ $CURRENT_BRANCH == "dev" ]]; then
-  docker tag $IMAGE_NAME ashwin31081999/dev:latest
-  echo "Tagged image as ashwin31081999/dev:latest"
-else
-  echo "Not on main or dev branch. Exiting."
-  exit 1
-fi
 
