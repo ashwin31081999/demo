d7ae5c79aed8a6b1bde70246188afeb6eba047ca
ashwin31081999 <163541950+ashwin31081999@users.noreply.github.com>
2024-06-23 21:25:55 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Push the Docker image to the appropriate repository
-if [[ $(git rev-parse --abbrev-ref HEAD) == "main" ]]; then
+if [[ $(git rev-parse --abbrev-ref HEAD) == "master" ]]; then
   docker push ashwin31081999/prod:latest
 elif [[ $(git rev-parse --abbrev-ref HEAD) == "dev" ]]; then
   docker push ashwin31081999/dev:latest
 else
-  echo "Not on main or dev branch. Exiting."
+  echo "Not on master or dev branch. Exiting."
   exit 1
 fi
 
