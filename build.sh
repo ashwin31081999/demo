e85d9717bbc917cd8fc6ad8f100c32caa56d3694
ashwin31081999 <163541950+ashwin31081999@users.noreply.github.com>
2024-06-27 17:16:28 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+#login into dockerhub:
+docker login -u $DOCKER_USER -p $DOCKER_PASS
 
-# Remove any existing image with the same name
-docker rmi -f my-nginx:latest
 
-# Build the Docker image
-docker build -t my-nginx:latest .
+#building the image:
+docker build -t nodejs .
+docker images
+
+
+#stopping the already running container:
+docker stop nodejs
+docker rm nodejs
 
 
