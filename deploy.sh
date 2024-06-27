865520bd1f7783751f7150ef4b39c94cb337c051
ashwin31081999 <163541950+ashwin31081999@users.noreply.github.com>
2024-06-27 22:19:00 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,9 @@
 if [[ $(git rev-parse --abbrev-ref HEAD) == "master" ]]; then
   docker tag nodejs $DOCKER_BFLASK_IMAGE1
   docker push $DOCKER_BFLASK_IMAGE1
-elif [[ $(git rev-parse --abbrev-ref HEAD) == "dev" ]]; then
+else [[ $(git rev-parse --abbrev-ref HEAD) == "dev" ]]; then
   docker tag nodejs $DOCKER_BFLASK_IMAGE2
   docker push $DOCKER_BFLASK_IMAGE2
-else
-  echo "Not on master or dev branch. Exiting."
-  exit 1
 fi
 
 
