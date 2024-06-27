2e880928c0f11a05c81cc3b4100f0467c29c391b
ashwin31081999 <163541950+ashwin31081999@users.noreply.github.com>
2024-06-27 19:05:49 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+# Deploy using docker-compose
+docker-compose up -d
 # Push the Docker image to the appropriate repository
 if [[ $(git rev-parse --abbrev-ref HEAD) == "master" ]]; then
   docker tag nodejs ashwin31081999/prod:latest
