13bf67d26dde306e9374721b5665147acfbc4314
ashwin31081999 <163541950+ashwin31081999@users.noreply.github.com>
2024-06-28 13:05:28 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-
+#docker login
 docker login -u $DOCKER_USER -p $DOCKER_PASS
-
+#stop and remove all containers
+docker rm -f $(docker ps -a -q)
+#remove all the images
+docker rmi $(docker images -aq)
 #building the image:
 docker build -t nodejs .
+#print the images
 docker images
+#creating containers
 docker-compose up -d
 
